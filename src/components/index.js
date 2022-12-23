6f42bc5e64adfb96d9887e1e2b56ca65df85968e
DOTL-Amir-R <raeisi.amirrr@gmail.com>
2022-12-23 14:25:23 +0330

    HomeV06

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,9 @@
 export * from 'components/shared/LyricsDrawer';
 export * from 'components/shared/musicPlayer';
+
+
+
+
 export * from 'components/shared/TitleCreator';
 export * from 'components/MusicCardHome';
 export * from 'components/SliderPictureCreator';
