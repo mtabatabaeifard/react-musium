127fbb5ff1d422fe66f0f478177610dd3f9e2735
Reza NezhadSalari <reza.9salari6@gmail.com>
2022-12-27 11:49:30 +0330

    Header va Footer musium

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,22 +1,33 @@
 import { Box } from '@mui/material';
+import Footer from 'layout/Footer';
+import Header from 'layout/Header';
 import React from 'react';
 import { ContinueListening } from './continueListening';
 import { FirstSlider } from './FirstSlider';
 import { SecondSlider } from './SecondSlider';
 
 function Home() {
-    return (
-        <Box
-            sx={{
-                backgroundColor: '#000000',
-                width: '422px',
-                margin: '0 auto',
-            }}>
-            <ContinueListening />
-            <FirstSlider />
-            <SecondSlider />
-        </Box>
-    );
+  return (
+    <>
+    <Box sx={{
+          backgroundColor: '#000000',
+          width: '422px',
+          margin: '0 auto'
+        }}>
+        <Header />   
+        <ContinueListening />
+        <FirstSlider />
+        <SecondSlider />
+      </Box>
+      <Box sx={{
+          backgroundColor: '#000000',
+          width: '422px',
+          margin: '0 auto'
+        }}>
+      <Footer />
+      </Box>
+    </>
+  );
 }
 
 export default Home;
