eb6d41222f054434b948ce33c4e1e1953aa00afe
Seyyed Mohammad Hossein Tabatabaeifard <mohammadirani469@gmail.com>
2023-01-04 11:43:21 +0330

    fixed eslint no-console warnings

--- src/pages/newPassword/singup/form/index.jsx
+++ src/pages/newPassword/singup/form/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import IconButton from '@mui/material/IconButton';
 import { Alert, Box, FormControl, InputAdornment } from '@mui/material';
 import { TextField } from 'components/shared/TextField';
